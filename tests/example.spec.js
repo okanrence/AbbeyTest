f4d7ff9d8a5f9ce39cec69911212a3cb00feb869
AbiodunAyesa <Abeybanj@yahoo.com>
2024-03-09 18:17:57 -0700

    automation for Login page CS-20

---
+++ tests/example.spec.js
@@ -0,0 +1,20 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Validate navigation to test url', async ({ page }) => {
+  await page.goto('https://google.com/');
+
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Google/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
